78e00722ec5da8fd874c9576bc9e3f15a1557dbb
UfU <universalflowuniversity@gmail.com>
2018-06-07 19:18:44 -0400

    Update and rename index.js to server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+// Modules
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Set port for server
+const port = process.env.PORT || 3100;
+
+// Express set up
+let app = express();
+app.use(bodyParser.json());
+
+
+
+app.post('/webhook', (req, res, next) => {
+
+    let action = req.body.result.action;
+    let message = action === 'get.wp.content' ? `Hey, our webhook is connected!` : `Sorry, I didn't get that`;
+
+    res.send({
+        speech: message,
+        displayText: message,
+        source: 'wp-webhook',
+    });
+
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
+
+
+
